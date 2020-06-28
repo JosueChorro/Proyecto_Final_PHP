8049b602fd42ee732af2e5dffb29071b3de086d7
palaciosky <63530913+palaciosky@users.noreply.github.com>
2020-06-28 12:08:14 -0600

    otra prueba

--- controller/ClienteController.php
+++ controller/ClienteController.php
@@ -3,7 +3,7 @@
 class ClienteController{
        
     public function Index(){
-        //muestra todas las partes de la vista home
+        //muestra todas las partes de la vista homes
         require_once 'view/pages/header.php';
         require_once 'view/pages/read/read_customer.php';
         require_once 'view/pages/foot_read.php';
